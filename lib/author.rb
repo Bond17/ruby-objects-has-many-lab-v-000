8a168c61c38c5c85677a1bbe127d6591c5081188
Robert Watson <spartan83@gmail.com>
2017-12-31 19:09:03 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,5 +1,6 @@
 class Author
 
+@@post_count =0
 attr_accessor :name
 
 def initialize(name)
@@ -10,13 +11,20 @@ end
 def posts
   @posts
 end
+
 def add_post(post)
   @posts<<post
   post.author = self
 end
+
 def add_post_by_title(title)
   post = Post.new(title)
   @posts<<post
   post.author = self
 end
+
+def post_count
+  @@post_count
+end
+
 end
