6af5af2b486d1aa3140d081f6e66323de56a08f8
Robert Watson <spartan83@gmail.com>
2017-12-31 18:41:38 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,27 @@
 class Artist
 attr_accessor :name
+@@song_count= 0
+
 
 def initialize(name)
 @name = name
 @songs=[]
+end
+
+def songs
+
+end
+
+def add_song
+
+end
+
+def add_song_by_name
+
+end
+
+def song_count
+
 end
 
 end
