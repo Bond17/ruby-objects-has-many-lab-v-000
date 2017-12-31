3b6079680b1917b3895d39382343f77a3500a6bb
Robert Watson <spartan83@gmail.com>
2017-12-31 18:51:26 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,14 +15,14 @@ end
 def add_song(song)
   @songs << song
   song.artist=self
-  song_count+=1
+  @@song_count+=1
 end
 
 def add_song_by_name(name)
   song= Song.new(name)
   @songs << song
   song.artist=self
-  song_count+=1
+  @@song_count+=1
 
 end
 
